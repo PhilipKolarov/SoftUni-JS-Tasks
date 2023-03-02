0397abc7943bcdd73703c7ae18aa5daa0cdc8730
Philip Kolarov <102211673+PhilipKolarov@users.noreply.github.com>
2023-03-02 11:26:19 +0200

    Add files via upload

---
+++ excellent-grade.js
@@ -0,0 +1,12 @@
+function gradeChecker(grade) {
+    if (grade >= 5.50) {
+        console.log('Excellent')
+    }
+
+    else {
+        console.log('Not excellent')
+    }
+}
+
+gradeChecker(5.40)
+gradeChecker(5.60)

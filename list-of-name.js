99dee8052fb350bd441c70a0fa1dd21248f0ae41
Philip Kolarov <102211673+PhilipKolarov@users.noreply.github.com>
2024-07-01 22:12:06 +0300

    Delete list-of-name.js

--- list-of-name.js
+++
@@ -1,8 +0,0 @@
-function addItem() {
-    const ulContainer = document.getElementById('items');
-    const input = document.getElementById('newItemText');
-    const newLi = document.createElemenet('li');
-    newLi.textContent = input.value;
-    ulContainer.appendChild(newLi);
-    input.value = '';
-}

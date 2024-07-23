192e8309a88e414d6dea080cd10dd79bf3f4155a
Philip Kolarov <102211673+PhilipKolarov@users.noreply.github.com>
2024-07-23 23:01:32 +0300

    Delete reverse-string.js

--- reverse-string.js
+++
@@ -1,8 +0,0 @@
-function reversedChars (a, b, c) {
-    myArray = [a, b, c];
-    myReversedArray = myArray.reverse();
-    myJoinedArray = myReversedArray.join(" ")
-    console.log(myJoinedArray)
-}
-
-reversedChars('x', 'y', 'z')
